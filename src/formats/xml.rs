e498373f420f7569c5f987c28526b66c73d1f604
Richard Neumann <mail@richard-neumann.de>
2023-06-24 18:48:24 +0200

    Add to_xml_pretty()

--- src/formats/xml.rs
+++ src/formats/xml.rs
@@ -1,6 +1,8 @@
 #[cfg(feature = "xml")]
 pub mod featured {
     use crate::Error;
+    use quick_xml::events::Event;
+    use quick_xml::{Reader, Writer};
     use serde::de::DeserializeOwned;
     use serde::Serialize;
     use std::fmt::Write;
@@ -95,6 +97,33 @@ pub mod featured {
         /// Returns an `serde_rw::Error` in case the serialization fails.
         fn to_xml(&self) -> Result<String, Error> {
             quick_xml::se::to_string(self).map_err(|error| Error::SerdeError(error.to_string()))
+        }
+
+        /// Return object as a pretty serialized XML string
+        /// # Errors
+        /// Returns an `serde_rw::Error` in case the serialization fails.
+        fn to_xml_pretty(&self, indent_char: u8, indent_size: usize) -> Result<String, Error> {
+            let xml = self.to_xml()?;
+            let mut reader = Reader::from_str(&xml);
+            reader.trim_text(true);
+            let mut writer = Writer::new_with_indent(Vec::new(), indent_char, indent_size);
+
+            loop {
+                let ev = reader.read_event();
+
+                match ev {
+                    Ok(Event::Eof) => break,
+                    Ok(event) => {
+                        if let Err(error) = writer.write_event(event) {
+                            return Err(Error::SerdeError(error.to_string()));
+                        }
+                    }
+                    Err(e) => return Err(Error::SerdeError(e.to_string())),
+                }
+            }
+
+            String::from_utf8(writer.into_inner())
+                .map_err(|error| Error::SerdeError(error.to_string()))
         }
 
         /// Writes object as serialized XML string to a file
